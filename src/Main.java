8ecb28f6d10a1a3a600f69d19be2a4ee2f5ea1cc
Florjetë Kuka <59765295+florjete@users.noreply.github.com>
2025-05-18 01:48:50 +0200

    Shtimi i funksionalitetit kryesor në Main për inicializimin e sesionit dhe nisjen e upload/download

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import client.ClientHandler;
+import models.TransferSession;
+import models.*;
+
+import javax.crypto.SecretKey;
+import java.io.File;
+import java.net.Socket;
+import java.security.KeyPair;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try {
+            KeyPair keyPair = RSAUtil.generateKeyPair();
+            SecretKey aesKey = AESUtil.generateAESKey();
+            File file = new File("file_to_send.txt");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            Socket socket = new Socket("localhost", 8080);
+            TransferSession session = new TransferSession(keyPair, aesKey, socket, file);
+
+            boolean doUpload = true; // Change to false to test download
+
+            if (doUpload) {
+                ClientHandler.upload(session);
+            } else {
+                ClientHandler.download(session, "downloaded_file.txt");
+            }
+
+            socket.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
