9ed628c06b24ee6389b531664b2b0a88ab704e7f
Flon15 <kastratiflon@gmail.com>
2025-05-17 22:40:22 +0200

    enkriptimin dhe dekriptimin AES-GCM.

--- src/server/AESFileEncryption.java
+++ src/server/AESFileEncryption.java
@@ -1,4 +1,42 @@
 package server;
-
+import javax.crypto.Cipher;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.ByteArrayOutputStream;
+import java.security.SecureRandom;
 public class AESFileEncryption {
+    private static final String ALGORITHM = "AES/GCM/NoPadding";
+    private static final int TAG_LENGTH_BIT = 128;
+    private static final int IV_LENGTH_BYTE = 12;
+
+    //enkriptimi
+    public static byte[] encrypt(byte[] keyBytes, byte[] plaintext) throws Exception {
+        byte[] iv = new byte[IV_LENGTH_BYTE];
+        new SecureRandom().nextBytes(iv);
+
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
+        GCMParameterSpec gcmSpec = new GCMParameterSpec(TAG_LENGTH_BIT, iv);
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, gcmSpec);
+
+        byte[] encrypted = cipher.doFinal(plaintext);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        out.write(iv);
+        out.write(encrypted);
+        return out.toByteArray();
+    }
+
+  //dekriptimi
+    public static byte[] decrypt(byte[] keyBytes, byte[] encryptedData) throws Exception {
+        byte[] iv = new byte[IV_LENGTH_BYTE];
+        System.arraycopy(encryptedData, 0, iv, 0, iv.length);
+        byte[] cipherText = new byte[encryptedData.length - iv.length];
+        System.arraycopy(encryptedData, iv.length, cipherText, 0, cipherText.length);
+
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
+        GCMParameterSpec gcmSpec = new GCMParameterSpec(TAG_LENGTH_BIT, iv);
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, gcmSpec);
+        return cipher.doFinal(cipherText);
+    }
 }
