353fb2c56b916c619921e7a217b67b4f042a268c
Flon15 <kastratiflon@gmail.com>
2025-05-17 22:51:39 +0200

    krijimi i file storage per ruajtje dhe lexim nga disku

--- src/server/FileStorage.java
+++ src/server/FileStorage.java
@@ -1,4 +1,33 @@
 package server;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FileStorage {
+    private final Path storageDir;
+
+    public FileStorage(Path storageDir) throws IOException {
+        this.storageDir = storageDir;
+        if (!Files.exists(storageDir)) {
+            Files.createDirectories(storageDir);
+        }
+    }
+
+
+    public void saveFile(String filename, byte[] data) throws IOException {
+        Path target = storageDir.resolve(filename);
+        Files.write(target, data);
+    }
+
+
+    public byte[] readFile(String filename) throws IOException {
+        Path target = storageDir.resolve(filename);
+        return Files.readAllBytes(target);
+    }
+
+
+    public boolean exists(String filename) {
+        return Files.exists(storageDir.resolve(filename));
+    }
 }
