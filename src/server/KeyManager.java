cd97a67f355be26c88d5edaf3ded84473f9bd2e3
Flon15 <kastratiflon@gmail.com>
2025-05-17 23:29:46 +0200

    shtimi i metodes decrypt with private key

--- src/server/KeyManager.java
+++ src/server/KeyManager.java
@@ -1,10 +1,13 @@
 package server;
 
+import javax.crypto.Cipher;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import javax.crypto.spec.OAEPParameterSpec;
+
 
 public class KeyManager {
 
@@ -30,5 +33,11 @@ public class KeyManager {
     // ➡️ Kyçi privat i serverit
     public PrivateKey getPrivateKey() {
         return rsaKeyPair.getPrivate();
+    }
+
+    public byte[] decryptWithPrivateKey(byte[] data) throws Exception {
+        Cipher rsa = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+        rsa.init(Cipher.DECRYPT_MODE, rsaKeyPair.getPrivate());
+        return rsa.doFinal(data);
     }
 }
