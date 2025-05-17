7f58c6e331ae04d930b22671896bd88df2336321
Florjetë Kuka <59765295+florjete@users.noreply.github.com>
2025-05-18 01:47:10 +0200

    Krijimi i modelit TransferSession për menaxhimin e sesioneve të transferimit

--- src/models/TransferSession.java
+++ src/models/TransferSession.java
@@ -1,4 +1,37 @@
+// TransferSession.java
 package models;
 
+import javax.crypto.SecretKey;
+import java.io.File;
+import java.net.Socket;
+import java.security.KeyPair;
+
 public class TransferSession {
+    private KeyPair keyPair;
+    private SecretKey aesKey;
+    private Socket socket;
+    private File file;
+
+    public TransferSession(KeyPair keyPair, SecretKey aesKey, Socket socket, File file) {
+        this.keyPair = keyPair;
+        this.aesKey = aesKey;
+        this.socket = socket;
+        this.file = file;
+    }
+
+    public KeyPair getKeyPair() {
+        return keyPair;
+    }
+
+    public SecretKey getAesKey() {
+        return aesKey;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public File getFile() {
+        return file;
+    }
 }
