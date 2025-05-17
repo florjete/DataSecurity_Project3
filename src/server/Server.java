7e1530113934996e3a363f579002bf585adf6f2f
Flon15 <kastratiflon@gmail.com>
2025-05-17 23:32:58 +0200

    krijimi i serverit

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,4 +1,26 @@
 package server;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Path;
+
 public class Server {
+    private static final int PORT = 5555;
+
+    public static void main(String[] args) throws Exception {
+        // 1) Përgatit storage
+        FileStorage storage = new FileStorage(Path.of("server_storage"));
+        // 2) Gjenero çifte RSA
+        KeyManager keyManager = new KeyManager();
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Server po pret në portin " + PORT);
+
+            while (true) {
+                Socket clientSock = serverSocket.accept();
+                // 3) Shërbe klienët në thread të veçantë
+                new Thread(new ServerHandler(clientSock, keyManager, storage)).start();
+            }
+        }
+    }
 }
