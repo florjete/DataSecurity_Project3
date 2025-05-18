944f81d7f3aa13162400a0e040d6ba71cf52b79b
Flon15 <kastratiflon@gmail.com>
2025-05-18 22:36:33 +0200

    created Digital signature class

---
+++ src/client/DigitalSignature.java
@@ -0,0 +1,21 @@
+package client;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+
+public class DigitalSignature {
+    public static byte[] sign(byte[] data, PrivateKey privateKey) throws Exception {
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initSign(privateKey);
+        signature.update(data);
+        return signature.sign();
+    }
+
+    public static boolean verify(byte[] data, byte[] signatureBytes, PublicKey publicKey) throws Exception {
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initVerify(publicKey);
+        signature.update(data);
+        return signature.verify(signatureBytes);
+    }
+}
